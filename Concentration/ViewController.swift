66eb4dfa7052c5368e06d6b5c1a444cc1171f52f
PV <vladislav.y.popov@gmail.com>
2019-10-08 11:01:05 +0300

    first commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Владислав on 04/10/2019.
+//  Copyright © 2019 Владислав. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func TouchCard(_ sender: UIButton) {
+    }
+}
