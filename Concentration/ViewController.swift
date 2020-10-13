0fcf5e8f739a1b7815498e86431cdcf8b0231c6a
Vladislav <vladislav.y.popov@gmail.com>
2020-10-13 17:42:20 +0300

    11w

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -33,7 +33,7 @@ class ViewController: UIViewController {
             let card = game.cards[index]
             if card.isFaceUp {
                 button.setTitle (emoji (for:card), for: UIControl.State.normal)
-                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+                button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
             } else {
                 button.setTitle("", for: UIControl.State.normal)
                 button.backgroundColor = card.isMatched ? #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
